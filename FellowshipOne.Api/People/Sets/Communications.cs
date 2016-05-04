91509fc552f4ac5e5cc1fa5ab98c95e0fbbf9b67
Chad Meyer <chadmeyer@me.com>
2016-05-04 17:33:48 -0500

    Adding a few more tests around communications and adding ability to delete a comm for a household.

--- FellowshipOne.Api/People/Sets/Communications.cs
+++ FellowshipOne.Api/People/Sets/Communications.cs
@@ -9,14 +9,17 @@ namespace FellowshipOne.Api.People.Sets {
         private const string EDIT_URL = "/v1/communications/{0}";
         private const string CREATE_INDIVIDUAL_COMMUNICATION_URL = "/v1/people/{0}/communications";
         private const string CREATE_HOUSEHOLD_COMMUNICATION_URL = "/v1/households/{0}/communications";
+        private const string DELETE_INDIVIDUAL_COMMUNICATION_URL = "/v1/people/{0}/communications/{1}";
+        private const string DELETE_HOUSEHOLD_COMMUNICATION_URL = "/v1/households/{0}/communications/{1}";
 
         public Communications(OAuthTicket ticket, string baseUrl) : base(ticket, baseUrl, ContentType.XML) { }
+        private string editUrl = EDIT_URL;
 
         protected override string GetUrl { get { return GET_URL; } }
         protected override string GetChildUrl { get { return GET_CHILD_URL; } }
         protected override string GetChildListUrl { get { return CHILD_LIST_URL; } }
         protected override string CreateUrl { get { return CREATE_URL; } }
-        protected override string EditUrl { get { return EDIT_URL; } }
+        protected override string EditUrl { get { return editUrl; } }
 
         public Model.Communication CreateForPerson(int personID, Model.Communication entity) {
             var url = string.Format(CREATE_INDIVIDUAL_COMMUNICATION_URL, personID);
@@ -36,6 +39,12 @@ namespace FellowshipOne.Api.People.Sets {
         public Model.Communication CreateForHousehold(int householdID, Model.Communication entity, out string requestXml) {
             var url = string.Format(CREATE_HOUSEHOLD_COMMUNICATION_URL, householdID);
             return Create(entity, out requestXml, url);
+        }
+
+        public void DeleteForHousehold(int id, int householdID) {
+            editUrl = string.Format(DELETE_HOUSEHOLD_COMMUNICATION_URL, householdID, id);
+            Delete(id.ToString());
+            editUrl = EDIT_URL;
         }
     }
 }
