52b8f357058340c82e1f154efaeae5530968ce38
Shane Vent <shanevent@52projectsllc.com>
2016-06-22 18:44:57 -0500

    Added ability to delete attribute.

--- FellowshipOne.Api/People/Sets/PeopleAttributes.cs
+++ FellowshipOne.Api/People/Sets/PeopleAttributes.cs
@@ -11,10 +11,13 @@ namespace FellowshipOne.Api.People.Sets {
     public class PeopleAttributes : ApiSet<PersonAttribute> {
         private const string CHILD_LIST_URL = "/v1/people/{0}/attributes";
         private const string CREATE_PERSON_ATTRIBUTE_URL = "/v1/people/{0}/attributes";
+        private const string EDIT_URL = "/v1/people/{0}/attributes/{1}";
         private string _createUrl = string.Empty;
+        private string _editUrl = string.Empty;
 
         public PeopleAttributes(OAuthTicket ticket, string baseUrl) : base(ticket, baseUrl, ContentType.XML) { }
 
+        protected override string EditUrl { get { return this._editUrl; } }
         protected override string GetChildListUrl { get { return CHILD_LIST_URL; } }
         protected override string CreateUrl {
             get {
@@ -37,6 +40,11 @@ namespace FellowshipOne.Api.People.Sets {
         public PersonAttribute CreateForPerson(int personID, Model.PersonAttribute entity, out string requestXml) {
             this._createUrl = string.Format(CREATE_PERSON_ATTRIBUTE_URL, personID);
             return Create(entity, out requestXml);
+        }
+
+        public bool DeleteForPerson(int personID, Model.PersonAttribute entity) {
+            this._editUrl = string.Format(EDIT_URL, personID, entity.APIModelID);
+            return Delete(entity.APIModelID);
         }
     }
 }
